dcecb5b14adf0f58484ef2fb9a9bab7d36609665
Courtney Phillips <courtney@epicodus.com>
2017-09-15 10:15:56 -0700

    Craft fetchSongId() async action using isomorphic fetch library to make async API request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from "./../constants/ActionTypes";
+import v4 from "uuid/v4";
 
 export const nextLine = (songId) => ({
   type: types.NEXT_LINE,
@@ -14,3 +15,16 @@ export const selectSong = (songId) => ({
   type: types.SELECT_SONG,
   songId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    title = title.replace(" ", "_");
+    return fetch("http://api.musixmatch.com/ws/1.1/track.search?&q_track=" + title + "&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE").then(
+      response => response.json(),
+      error => console.log("An error occured.", error)
+    ).then(function(json) {
+      console.log("CHECK OUT THIS SWEET API RESPONSE:", json)
+    });
+  };
+}
