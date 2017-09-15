8f4cb9bc9fb20f97babce0e7e328d6146a214949
Courtney Phillips <courtney@epicodus.com>
2017-09-15 11:29:08 -0700

    Parse necessary info from API response.

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,7 +27,7 @@ export function fetchSongId(title) {
     const localSongId = v4();
     dispatch(requestSong(title, localSongId));
     title = title.replace(" ", "_");
-    return fetch("http://api.musixmatch.com/ws/1.1/track.search?&q_track=" + title + "&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE").then(
+    return fetch("http://api.musixmatch.com/ws/1.1/track.search?&q_track=" + title + "&page_size=1&s_track_rating=desc&apikey=a8503c69d6322a8e9e7faaaa8afc05a1").then(
       response => response.json(),
       error => console.log("An error occured.", error)
     ).then(function(json) {
