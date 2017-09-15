6891a87627435c0159b12378138f7f77996763c3
Courtney Phillips <courtney@epicodus.com>
2017-09-15 11:30:58 -0700

    Create fetchLyrics() async action to make second API request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,7 +31,23 @@ export function fetchSongId(title) {
       response => response.json(),
       error => console.log("An error occured.", error)
     ).then(function(json) {
-      console.log("CHECK OUT THIS SWEET API RESPONSE:", json)
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log("We couldn't locate a song under that ID!");
+      }
     });
   };
+}
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + musicMatchId + "&apikey=a8503c69d6322a8e9e7faaaa8afc05a1").then(
+    response => response.json(),
+    error => console.log("An error occured.", error)
+  ).then(function(json) {
+    console.log("HEY WOW, A SECOND API RESPONSE!", json);
+  });
 }
