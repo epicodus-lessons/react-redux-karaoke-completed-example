05dabd48b2c03fc407234a1edb9b0ebe303adb67
Courtney Phillips <courtney@epicodus.com>
2017-09-15 10:51:22 -0700

    Refactor form in SongSearch component to use bound event handler method to dispatch necessary actions.

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -5,26 +5,26 @@ import { connect } from "react-redux";
 
 class SongSearch extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let { _title } = this.refs;
+    if (!_title.value.trim()) {
+      return;
+    }
+    this.props.dispatch(fetchSongId(_title.value.trim());
+    _title.value = "";
+  }
+
   render(){
-    let input;
     return (
       <div className = {styles.songSearch}>
-        <form onSubmit={e => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            return;
-          }
-
-          // this is where custom logic goes
-          console.log("SEARCHED SONG:");
-          console.log(input.value.trim());
-          //
-
-          input.value = "";
-        }}>
-        <input placeholder="Song Title" ref={node => {
-            input = node;
-          }}></input>
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="Song Title" ref="_title"></input>
           <button>search</button>
         </form>
       </div>
